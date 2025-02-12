ea8e56c81541306df85a23097b65bd086d2f7bbc
kent <164713332@qq.com>
2025-02-12 04:08:57 +0000

    Fix bug.

--- src/Plugin/Commerce/PaymentGateway/Alipay.php
+++ src/Plugin/Commerce/PaymentGateway/Alipay.php
@@ -388,11 +388,9 @@ class Alipay extends OffsitePaymentGatewayBase implements
     $refund->setRefundNumber($this->getEntityTrackingId($refund));
     $refund->save();
 
-    $rs = $this->callRefundApi($refund->getRefundNumber(), $refund->getAmount()->getNumber());
+    $rs = $this->callRefundApi($refund->getPayment()->trade_tracking_id->value, $refund->getAmount()->getNumber());
 
     $refund->setRemoteId($rs['trade_no']);
-    $refund->getState()->applyTransitionById('complete');
-    $refund->save();
 
     $this->updatePaymentRefundedAmountAndState($refund->getPayment(), $refund->getAmount());
   }
@@ -442,7 +440,7 @@ class Alipay extends OffsitePaymentGatewayBase implements
     $trade_no = $rs->tradeNo;
     $out_trade_no = $rs->outTradeNo;
     // $body: {"alipay_trade_refund_response":{"code":"10000","msg":"Success","buyer_logon_id":"129***@qq.com","fund_change":"Y","gmt_refund_pay":"2025-02-10 14:22:14","out_trade_no":"202502101417081282-21","refund_detail_item_list":[{"amount":"0.01","fund_channel":"ALIPAYACCOUNT"}],"refund_fee":"0.01","send_back_fee":"0.01","trade_no":"2025021022001472541430920598","buyer_open_id":"0543v3Ch5stl8XBZW845p4RmmVl8W3YM-Y4eCvAesSaz4Aa"},"alipay_cert_sn":"dda2416c0aa167d93fed1e01e3dca2b1","sign":"gPQf/nUQZaKJdcPwaTf/YGZWYmf6WGT3yFBswdEMQ4usqa6rSIwTT4NHoEFuJKTLH5Q2cCgZ25xiXRMzjZuZvlnrfCcb1GvCXs54+QN3sKwjWVjkIeMnXSE0kS64f4FTtmWAOgxIu7TN+uG3rYmAN+cRox+QCboaoTl32EuLjmjW3YVjx8nk/MZKdgBX9yZHmJNuRkgSWhj3yepDjBC1ptoVmpUjsThLAzs53kXIGsxAuclnx5cdXRL+XrQ3e6tUr/Pr+3iO716wDw/+JHlz7QAxaj9ZO1i9wYJOZ84t/OxA39bNUlqTVRG629AdnRbNi5jFJEoBFXMozrqq5ZvhfQ=="}
-    $responseBody = json_encode($rs->httpBody);
+    $responseBody = json_decode($rs->httpBody, TRUE);
     if ($this->verifyContent(json_encode($responseBody['alipay_trade_refund_response']), $responseBody['sign'], $responseBody['alipay_cert_sn'])) {
       return $responseBody['alipay_trade_refund_response'];
     }
